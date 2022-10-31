975a3bd4ecda8e2fffd47c9550e24500a92eed53
parthh7555 <pparthh75@gmail.com>
2022-10-31 23:19:58 +0530

    zenish 31/10/2022 11:20

--- lib/utilities/string.dart
+++ lib/utilities/string.dart
@@ -1,4 +1,14 @@
 class CS {
   String igli = "IGLI";
   static String igliFinancial = "IGLI Financial";
+
+  static String save="Save";
+  static String needHelp="Need Help";
+  static String weWillBeHappyToAssistYou="We will be happy to assist you.";
+  static String selectEntity="Select Entity";
+  static String whatIsTheQueryAbout="What is the query about?";
+  static String typeYourMessageHere="Type your message here";
+
+  ///dummyText
+  static String dEmail="test@gmaiul.com";
 }
